4fbe74d19f1dcb77669a49c274ec9afa362a47c0
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-30 11:43:49 +0530

    Insert

--- Program.cs
+++ Program.cs
@@ -18,6 +18,14 @@ namespace HashTable
             {
                 Console.WriteLine("{0} and {1} ", ele.Key, ele.Value);
             }
+            Hashtable h1 = new Hashtable() { { 1, "Paranoids" } , { 2, "are" }, { 3, "Paranoid" },
+            { 4, "because" } ,{ 5, "they" } ,{ 6, "Paranoid" } ,{ 7, "but" } ,{ 8, "because" } ,
+            { 9, "they" } ,{ 10, "keep" } ,{ 11, "putting" } ,{ 12, "themselves" } ,{ 13, "deliberately" } ,
+            { 14, "into" } ,{ 15, "Paranoid" } ,{ 16, "avoidable" } ,{ 17, "situations" } };
+            foreach (DictionaryEntry ele1 in h)
+            {
+                Console.WriteLine("{0} and {1} ", ele1.Key, ele1.Value);
+            }
         }
     }
 }
